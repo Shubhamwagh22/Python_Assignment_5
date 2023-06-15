de37156eb73b1da42640b6f6916896a18de3067c
Shubhamwagh22 <131356151+Shubhamwagh22@users.noreply.github.com>
2023-06-15 18:27:33 +0530

    Assignment_5.1.py

---
+++ Assignment_5.1.py
@@ -0,0 +1,14 @@
+# Challenge 1: Square Numbers and Return Their Sum
+
+class Point:
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def sqSum(self):
+        return self.x ** 2 + self.y ** 2 + self.z ** 2
+
+Point1 = Point(1,3,5)
+print(Point1.sqSum())
